8aa56936f30b948c622afe316bbf2b24183944ae
Anshika0805 <95037404+Anshika0805@users.noreply.github.com>
2022-10-21 04:37:07 +0530

    Update xpattern.java

--- xpattern.java
+++ xpattern.java
@@ -20,3 +20,26 @@ public class Main {
         }
     }
 }
+Mthd 2 : to make a single point of intersection there should be odd rows hence multiply rows by 2 and subtract 1.
+    
+    import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int num =  scn.nextInt();
+        int k = 2*num-1;
+        for(int i = 1; i<=k ;i++) {
+            for(int j = 1; j <= k; j++) {
+                if(i==j || j+i == k+1) {
+                    System.out.print("*");
+                }
+                else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
