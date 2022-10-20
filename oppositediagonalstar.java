33df47af0c74982c3a472ffc7a708cad0f92d9ea
Anshika0805 <95037404+Anshika0805@users.noreply.github.com>
2022-10-21 02:56:16 +0530

    Create oppositediagonalstar.java

---
+++ oppositediagonalstar.java
@@ -0,0 +1,37 @@
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int num =  scn.nextInt();
+        int st = 1;
+        int sp = num-1;
+        for(int i = 1; i<=num ;i++) {
+            
+            for(int j = 1; j <= sp ; j++) {
+                System.out.print("$\t");
+            }
+            for(int k =1; k <= st;k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+            sp--;                
+        }
+    }
+}
+Mthd 2:
+
+for(i=1; i<=num;i++) {
+  for(j=1;j<=num;j++) {
+    if(i+j == n+1) {
+      System.out.print("*\t");
+    } 
+    else {
+      System.out.print("\t");
+    }
+    System.out.println();
+  }
+}
