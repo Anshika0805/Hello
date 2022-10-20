abd0c0da45bbf980a2a46e47294a2be9f523cc6c
Anshika0805 <95037404+Anshika0805@users.noreply.github.com>
2022-10-21 02:22:42 +0530

    Update diagonalStar.java

--- diagonalStar.java
+++ diagonalStar.java
@@ -20,12 +20,6 @@ public class Main {
             System.out.println();
             sp++;
                 
-        } 
-        
-            
-            
-        
-        
+        }
     }
-
 }
